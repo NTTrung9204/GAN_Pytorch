b142b41f59f568a0ecebfa572a52424ce0bac8d9
Trung Nguyễn <trung9204@gmail.com>
2025-03-18 20:20:23 +0700

    [Feat]: Add batchnorm layer

--- conditional_gan/generator_net.py
+++ conditional_gan/generator_net.py
@@ -13,15 +13,25 @@ class Generator(nn.Module):
         self.deconv_2 = nn.ConvTranspose2d(64, 32, (2, 2), 2)
         self.deconv_3 = nn.Conv2d(32, 1, (1, 1))
 
+        self.relu = nn.LeakyReLU(0.2)
+
+        self.drop = nn.Dropout(0.5)
+
     def forward(self, noise, label):
         x = self.linear_1(noise)
-        x = torch.reshape(x, (128, 7, 7))
+        x = self.relu(x)
+        x = torch.reshape(x, (-1, 128, 7, 7))
+        x = self.drop(x)
 
         y = self.embedding_1(label)
+        y = self.relu(y)
+        y = self.drop(y)
         y = self.linear_2(y)
-        y = torch.reshape(y, (1, 7, 7))
+        y = torch.reshape(y, (-1, 1, 7, 7))
+        y = self.relu(y)
+        y = self.drop(y)
 
-        z = torch.concat((x, y))
+        z = torch.concat((x, y), dim=1)
 
         z = self.deconv_1(z)
         z = self.deconv_2(z)
